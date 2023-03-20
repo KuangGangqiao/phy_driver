989500b7a1601a4bca8a0714629add313dc2fdaa
Gangqiao Kuang <gqkuang@jlsemi.com>
2023-03-20 14:56:18 +0800

    feat(tool): update chechpatch script for check code style

--- tool/checkpatch/checkpatch.sh
+++ tool/checkpatch/checkpatch.sh
@@ -1,5 +1,5 @@
 ROOT=$(git rev-parse --show-toplevel)
 CODE_PATH=source/phy_driver/jlsemi
 SYS_HEAD_PATH=$(uname -r)
-CHECKPATCH=/usr/src/linux-headers-${SYS_HEAD_PATH}/scripts/checkpatch.pl
+CHECKPATCH=./tool/checkpatch/checkpatch_5_4.pl
 ${CHECKPATCH} -f ${ROOT}/${CODE_PATH}/jlsemi* --no-tree
