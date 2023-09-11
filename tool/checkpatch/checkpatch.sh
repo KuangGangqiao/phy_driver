5e1a54b0720b08b7b89e22fe72aa4a1f3ae0cb7d
Gangqiao Kuang <gqkuang@jlsemi.com>
2023-09-11 11:57:20 +0800

    feat(checkpatch): add ignore data type
    - 1.LINUX_VERSION_CODE
    - 2.MACRO_WITH_FLOW_CONTROL
    - 3.PREFER_PR_LEVEL

--- tool/checkpatch/checkpatch.sh
+++ tool/checkpatch/checkpatch.sh
@@ -1,5 +1,6 @@
 ROOT=$(git rev-parse --show-toplevel)
 CODE_PATH=source/phy_driver/jlsemi
+WARNING_TYPE=LINUX_VERSION_CODE,MACRO_WITH_FLOW_CONTROL,PREFER_PR_LEVEL
 SYS_HEAD_PATH=$(uname -r)
 CHECKPATCH=./tool/checkpatch/checkpatch_5_4.pl
-${CHECKPATCH} -f ${ROOT}/${CODE_PATH}/jlsemi* --no-tree
+${CHECKPATCH} -f ${ROOT}/${CODE_PATH}/jlsemi* --no-tree --ignore ${WARNING_TYPE} --show-types
