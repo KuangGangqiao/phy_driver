88209a18d2fac4a9e4e8664f303399031c9ab0c9
Gangqiao Kuang <gqkuang@jlsemi.com>
2023-09-12 17:13:35 +0800

    feat(downloads) add downloads load script

---
+++ downloads/prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER=sudo
+# for libreoffice
+${USER} apt-get install libreoffice-java-common -y
+${USER} apt-get install libreoffice-common -y
+${USER} apt-get install libreoffice-writer -y
+# for cross compile kernel
+${USER} apt-get install libncurses5-dev libssl-dev -y
+${USER} apt-get install build-essential openssl -y
+${USER} apt-get install zlibc minizip -y
+${USER} apt-get install libidn11-dev libidn11 -y
+${USER} apt-get install bison -y
+${USER} apt-get install flex -y
+${USER} apt-get install libelf-dev -y
+${USER} apt-get install libelf-devel -y
+${USER} apt-get install elfutils-libelf-devel -y
+${USER} apt-get install flex -y
+${USER} apt-get install bison -y
+
