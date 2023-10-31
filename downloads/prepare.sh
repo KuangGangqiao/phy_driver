62227e71b0e8952f8bce6a8b6593987f312de05f
Gangqiao Kuang <gqkuang@jlsemi.com>
2023-10-31 13:59:38 +0800

    chore(env): install java environment for liberoffice

--- downloads/prepare.sh
+++ downloads/prepare.sh
@@ -5,6 +5,7 @@ USER=sudo
 ${USER} apt-get install libreoffice-java-common -y
 ${USER} apt-get install libreoffice-common -y
 ${USER} apt-get install libreoffice-writer -y
+${USER} apt-get install openjdk-8-jre-headless -y
 # for cross compile kernel
 ${USER} apt-get install libncurses5-dev libssl-dev -y
 ${USER} apt-get install build-essential openssl -y
